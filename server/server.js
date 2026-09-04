a97bae8e2d734de00fb7fae65fb9b474d41a6101
borukva-news <borukva-news@gmail.com>
2026-09-04 22:00:24 +0300

    Allow moderation token header in CORS

--- server/server.js
+++ server/server.js
@@ -74,7 +74,7 @@ app.use(
       return callback(null, true); // Permissive CORS for public API
     },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-moderation-token'],
   })
 );
 
